e6854d32b6893d98dbcc8db993560de4349a9898
BahaBurrito <100386952+BahaBurrito@users.noreply.github.com>
2023-01-30 12:26:35 -0500

    made FastPlace Smoller

--- Horion/Module/Modules/FastPlace.cpp
+++ Horion/Module/Modules/FastPlace.cpp
@@ -4,37 +4,23 @@ FastPlace::FastPlace() : IModule(0x0, Category::WORLD, "Place Blocks faster") {
 	this->registerBoolSetting("Only Crystals", &this->onlyCrystals, this->onlyCrystals);
 }
 
-FastPlace::~FastPlace() {
-}
+FastPlace::~FastPlace() {}
 
 const char* FastPlace::getModuleName() {
-	return ("FastPlace");
+	return "FastPlace";
 }
 
-
 void FastPlace::onTick(GameMode* gm) {
-	auto player = Game.getLocalPlayer();
-	if (player == nullptr) return;
-
-	if (GameData::isRightClickDown()) {
+	if (auto player = Game.getLocalPlayer(); player != nullptr && GameData::isRightClickDown()) {
 		auto pStruct = Game.getClientInstance()->getLevel();
 		Vec3i block = pStruct->block.toVec3t();
 		int face = pStruct->blockSide;
 		int rayType = pStruct->rayHitType;
-
-		auto selectedItem = Game.getLocalPlayer()->getSelectedItem();
-		auto selectedItemId = Game.getLocalPlayer()->getSelectedItemId();
-		bool isCrystal = false;
-
 		if (!onlyCrystals && rayType == 0) {
-			if (selectedItemId == 615 && !selectedItem->count == 0 || selectedItemId == 616 && !selectedItem->count == 0 || selectedItemId == 629 && !selectedItem->count == 0) {
-				isCrystal = true;
-			} else {
-				isCrystal = false;
-			}
-
-			if (!isCrystal) {
-				if (selectedItem == nullptr || selectedItem->count == 0 || selectedItem->item == nullptr || !selectedItem->getItem()->isBlock())
+			if (auto selectedItem = player->getSelectedItem(); selectedItem != nullptr) {
+				auto selectedItemId = player->getSelectedItemId();
+				bool isCrystal = (selectedItemId == 615 || selectedItemId == 616 || selectedItemId == 629) && !selectedItem->count == 0;
+				if (!isCrystal && (!selectedItem->getItem() || !selectedItem->getItem()->isBlock() || selectedItem->count == 0))
 					return;
 			}
 		}
