0e48eb15b52238a393df83ede60b75c7abeaf096
BahaBurrito <100386952+BahaBurrito@users.noreply.github.com>
2023-02-04 10:55:46 -0500

    JESUS PRAISE THE LORD

--- Horion/Module/Modules/Jesus.cpp
+++ Horion/Module/Modules/Jesus.cpp
@@ -11,21 +11,16 @@ const char* Jesus::getModuleName() {
 }
 
 void Jesus::onTick(GameMode* gm) {
-	if (gm->player->isSneaking()) return;
+	if (gm->player->isSneaking() || !gm->player->isInWater() && !gm->player->hasEnteredWater() && !gm->player->isInLava()) return;
 
-	if (gm->player->hasEnteredWater()) {
-		gm->player->velocity.y = 0.06f;
-		gm->player->onGround = true;
-		wasInWater = true;
-	} else if (gm->player->isInWater() || gm->player->isInLava()) {
-		gm->player->velocity.y = 0.1f;
-		gm->player->onGround = true;
-		wasInWater = true;
-	} else {
-		if (wasInWater) {
-			wasInWater = false;
-			gm->player->velocity.x *= 1.2f;
-			gm->player->velocity.x *= 1.2f;
-		}
+	gm->player->velocity.y = 0.1f;
+	gm->player->onGround = true;
+	wasInWater = true;
+
+	if (!gm->player->isInWater() && !gm->player->isInLava() && wasInWater) {
+		wasInWater = false;
+		gm->player->velocity.x *= 1.2f;
+		gm->player->velocity.z *= 1.2f;
 	}
 }
+
